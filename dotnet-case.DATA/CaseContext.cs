b873a51730324e926346d69d078776e5ceaa83c5
Dale Cooper <keeperofgmachete@gmail.com>
2020-10-25 16:59:29 +0100

    add migration

--- dotnet-case.DATA/CaseContext.cs
+++ dotnet-case.DATA/CaseContext.cs
@@ -1,33 +1,44 @@
 ﻿using dotnet_case.BL.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 
 namespace dotnet_case.DATA
 {
     public class CaseContext : DbContext
     {
         public CaseContext(DbContextOptions<CaseContext> options)
-           : base(options)
-        {
-        }
-
+           : base(options) {}
         public DbSet<ArtistModel> Artists { get; set; }
         public DbSet<AlbumModel> Albums { get; set; }
         public DbSet<TrackModel> Tracks { get; set; }
+    }
 
-        //// According to Julie Lerman, EF Core expects you to explicitly specify 
-        //// a data provider and connection string. She adds it here in the context class.
-        //// However, she's not plugging EF into a web API. Her tutorial = just a console app.
+    // when using a migration command, EF Tools will try to obtain the DbContext from the app's service provider.
+    // however, the current .DATA project is a mere class library, and not an ASP.NET Core app. 
+    // also, the connection string is kept in the .API project's appsettings.json file.
 
-        //// Override virtual method OnConfiguring...
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    // Access UseSqlServer, which expects a connection string.
-        //    // UseSqlServer is available because we installed 
-        //    // Microsoft.EntityFrameworkCore.SqlServer
-        //    optionsBuilder.UseSqlServer(
-        //        "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = dotnet-caseDATA");
-        //    //base.OnConfiguring(optionsBuilder); // apparently not needed
-        //}
+    // bypass normal tools behavior with the IDesignTimeDbContextFactory<TContext>
+    // “If a class implementing this interface is found in either the same project as the derived DbContext 
+    // or in the application’s startup project, the tools bypass the other ways of creating the DbContext and 
+    // use the design-time factory instead.”
+
+    // the following code needs:
+    // -Microsoft.Extensions.Configuration.FileExtensions nuget package for SetBasePath
+    // -Microsoft.Extensions.Configuration.Json nuget package for AddJsonFile
+    // it then specifies the data provider as sql server and enters the conn. string as a parameter.
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CaseContext>
+    {
+        public CaseContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/../dotnet-case.API/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<CaseContext>();
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
+            builder.UseSqlServer(connectionString);
+            return new CaseContext(builder.Options);
+        }
     }
 }
---
+++ dotnet-case.DATA/Migrations/20201025155226_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_case.DATA.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    AlbumId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    ArtistId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.AlbumId);
+                    table.ForeignKey(
+                        name: "FK_Albums_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    TrackId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    AlbumId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.TrackId);
+                    table.ForeignKey(
+                        name: "FK_Tracks_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "AlbumId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_ArtistId",
+                table: "Albums",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_AlbumId",
+                table: "Tracks",
+                column: "AlbumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tracks");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
