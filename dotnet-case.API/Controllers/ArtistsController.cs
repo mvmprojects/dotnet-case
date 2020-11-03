70a0814f7943776c030faa1ca653f0015e563151
Dale Cooper <keeperofgmachete@gmail.com>
2020-11-03 20:28:46 +0100

    test wrapper object and new controller method

--- dotnet-case.API/Controllers/ArtistsController.cs
+++ dotnet-case.API/Controllers/ArtistsController.cs
@@ -25,12 +25,44 @@ namespace dotnet_case.API.Controllers
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        // GET: api/Artists 
+        // test GET: api/Artists 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ArtistDto>>> GetArtists() {
             
             var data = await _context.Artists.ToListAsync();
             return _mapper.Map<List<ArtistDto>>(data);
+
+            // todo test me - requires AutoMapper.QueryableExtensions
+            // requires the ORM to expose IQueryable
+            // https://docs.automapper.org/en/stable/Queryable-Extensions.html
+            // var data = await _context.Artists.ProjectTo<ArtistDto>().ToListAsync();
+        }
+
+        // TODO: GET for a wrapper object that adds total list size as int (in its constructor)
+        // forward slash in the route template is not required
+        [HttpGet("getlist")]
+        public IActionResult GetList()
+        {
+            List<ArtistModel> artistList = _context.Artists.ToList();
+            List<ArtistDto> mappedList = _mapper.Map<List<ArtistDto>>(artistList);
+            ArtistRequestWrapper wrapper = new ArtistRequestWrapper(mappedList);
+            return Ok(wrapper);
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return StatusCode(501);
+        }
+
+        public ActionResult Update()
+        {
+            return StatusCode(501);
+        }
+
+        public ActionResult Delete()
+        {
+            return StatusCode(501);
         }
     }
 }
---
+++ dotnet-case.API/Dtos/ArtistRequestWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_case.API.Dtos
+{
+    public class ArtistRequestWrapper
+    {
+        public List<ArtistDto> results { get; set; }
+        public int total { get; set; }
+
+        public ArtistRequestWrapper(List<ArtistDto> artistDtos)
+        {
+            this.results = artistDtos;
+            this.total = artistDtos.Count();
+        }
+    }
+}
